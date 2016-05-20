a72655a54b437ec84c5c0fb014b641421e6a78f7
gaokai <gaokai@qima-inc.comgit>
2016-05-20 15:09:44 +0800

    add blackhole_btn

--- src/sections/BlackHole.js
+++ src/sections/BlackHole.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './styles/blackhole.scss'
 import AnmElement from '../components/AnmElement'
 
-export default () =>
-  <div>
-    <AnmElement className="blackhole-bg" />
-    <AnmElement className="blackhole-text1" />
-    <AnmElement className="blackhole-text2" />
-    <AnmElement className="blackhole-text3" />
-  </div>
+export default class BlackHole extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hasEnter: false
+    }
+  }
+
+  render() {
+    const { hasEnter } = this.state
+
+    return (
+      <AnmElement className={`blackhole-bg ${hasEnter ? 'boom' : ''}`}>
+        <AnmElement
+          className="blackhole-btn"
+          onClick={() => this.setState({ hasEnter: true })}
+        />
+        <AnmElement className="blackhole-text1" />
+        <AnmElement className="blackhole-text2" />
+        <AnmElement className="blackhole-text3" />
+      </AnmElement>
+    )
+  }
+}
