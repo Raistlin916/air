f5d6cc5a9fe1382bd83f97525c4d73405bc27696
gaokai <gaokai@qima-inc.comgit>
2016-05-20 16:15:23 +0800

    improve

--- src/sections/Start3.js
+++ src/sections/Start3.js
@@ -3,7 +3,6 @@ import './styles/start3.scss'
 import AnmElement from '../components/AnmElement'
 
 export default () =>
-  <div>
-    <AnmElement className="section-start3-bg" />
+  <AnmElement className="section-start3-bg">
     <AnmElement className="section-start3-haze" />
-  </div>
+  </AnmElement>
