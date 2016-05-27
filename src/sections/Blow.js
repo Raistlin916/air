0d19f6003e66af91e8e9d63d9deb74e6e67cb148
Raistlin916 <raistlin916@gmail.com>
2016-05-27 14:40:35 +0800

    add blow page

--- src/sections/Blow.js
+++ src/sections/Blow.js
@@ -1,7 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
+import AnmElement from '../components/AnmElement'
+import './styles/blow.scss'
 
 export default class Blow extends Component {
+  static propTypes = {
+    nextPage: PropTypes.func
+  };
+
   render() {
-    return <div>Blow</div>
+    return (
+      <AnmElement className="blow-bg">
+        <AnmElement className="blow-fog2" />
+        <AnmElement className="blow-fog3" />
+        <AnmElement className="blow-text1" />
+        <AnmElement className="blow-text2" />
+        <AnmElement className="circle-btn" onClick={this.props.nextPage} />
+      </AnmElement>
+    )
   }
 }
