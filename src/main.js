200fc4aa0483c1c0482bd921a843923f99f9825c
Raistlin916 <raistlin916@gmail.com>
2016-12-22 22:20:43 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -6,9 +6,18 @@ import './global.scss'
 import App from './App.js'
 
 //import './utils/wx'
-import MusicPlayer from './utils/music_player'
-setTimeout(() => {
-  new MusicPlayer('http://img.yzcdn.cn/air_music_2.mp3')
-}, 1000)
+// import MusicPlayer from './utils/music_player'
+// setTimeout(() => {
+//   new MusicPlayer('http://img.yzcdn.cn/wind_blow.wav')
+// }, 1000)
+
+const audio = new Audio()
+document.body.addEventListener('touchstart', () => {
+  if (audio.duration > 0 && !audio.paused) {
+    return
+  }
+  audio.src = 'http://img.yzcdn.cn/air_music_2.mp3'
+  audio.play()
+})
 
 ReactDom.render(<App />, document.getElementById('container'))
